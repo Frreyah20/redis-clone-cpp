f81dd5acac8ad9889f3389e13afa1b06198bdfe2
Frreyah20 <frreyahbhhangale@gmail.com>
2026-06-23 10:23:50 +0000

    feat: add RESP support and automated unit tests

--- tests/database_tests.cpp
+++ tests/database_tests.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
 
 #include "../src/database/Database.h"
 
@@ -7,4 +9,37 @@ TEST(DatabaseTest, SetAndGet)
     Database db;
     db.set("name", "frreyah");
     EXPECT_EQ(db.get("name"),"frreyah");
+}
+
+TEST(DatabaseTest, Exists)
+{
+    Database db;
+    db.set("name", "frreyah");
+    EXPECT_TRUE(db.exists("name"));
+    EXPECT_FALSE(db.exists("missing"));
+}
+
+TEST(DatabaseTest, DeleteKey)
+{
+    Database db;
+    db.set("name", "frreyah");
+    EXPECT_TRUE(db.del("name"));
+    EXPECT_FALSE(db.exists("name"));
+}
+
+TEST(DatabaseTest, ExpireKey)
+{
+    Database db;
+    db.set("temp", "value");
+    EXPECT_TRUE(db.expire("temp", 1));
+}
+
+TEST(DatabaseTest, TTLExpiration)
+{
+    Database db;
+    db.set("temp", "value");
+    db.expire("temp", 1);
+    EXPECT_TRUE(db.exists("temp"));
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    EXPECT_FALSE(db.exists("temp"));
 }
